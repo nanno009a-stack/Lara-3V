7080c64ea0f3aa9e03b6026d4b120f350eab26a7
nanno009a-stack <nanno.009a@gmail.com>
2026-06-01 15:22:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94779062397' : process.env.OWNER_NUMBER, //ADD YOUR NUMBER
-    SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID, //ADD YOUR SSION ID
+    OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '+63 951 068 1297' : process.env.OWNER_NUMBER, //ADD YOUR NUMBER
+    SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.𝕃𝔸ℝ𝔸-𝕄𝔻=SdJShJLY#owgiMZXn7YipbFx1f8WXcadxbPhzwT-Zl32dbCwCZU4, //ADD YOUR SSION ID
     GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? '' : process.env.GITHUB_USER_NAME, //ADD YOUR GITHUB USERNAME
     GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN //ADD YOUR GITHUB AUTH TOKEN WITHOUT gph_ ,  example - G5OxxdvEbiBPWxm4A0xypQGlyCr4FS267ifz
 };
